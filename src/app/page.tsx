cef120237a93937bfcc693194e4e39c5847e6754
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2025-03-23 03:21:06 +0900

    feat: 카드 추가 및 차트 아이템 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import ChartList from '@/components/chart/list/list';
 
 export default function Home() {
   return (
-    <main className='flex h-dvh flex-col items-center'>
+    <main className='flex flex-col items-center overflow-hidden'>
       <Banner />
-      <article className='flex w-full flex-1 flex-col pt-1'>
+      <article className='flex w-full flex-1 flex-col overflow-hidden pt-1'>
         <h2 className='px-3 text-lg font-semibold'>글로벌 월드 차트 - 아티스트</h2>
         <Suspense fallback={<div>로딩중...</div>}>
           <ChartList />
--- src/components/chart/list/item.tsx
+++ src/components/chart/list/item.tsx
@@ -1,5 +1,47 @@
-const ChartItem = () => {
-  return <li></li>;
+'use client';
+
+import Image from 'next/image';
+import { Card, CardContent } from '@/components/ui/card';
+import type { ChartItem as ChartItemType } from '@/lib/types';
+import { useRouter } from 'next/navigation';
+
+type ChartItemProps = {} & ChartItemType;
+
+const ChartItem = (props: ChartItemProps) => {
+  const {
+    rank,
+    targetIdx,
+    targetName,
+    targetImg,
+    detail: { entertainment }
+  } = props;
+  const router = useRouter();
+
+  return (
+    <li
+      className='w-full px-1.5 hover:cursor-pointer'
+      onClick={() => router.push(`${process.env.NEXT_PUBLIC_DETAIL_URL}/${targetIdx}`)}
+    >
+      <Card className='bg-secondary/50 hover:bg-background/80 w-full'>
+        <CardContent className='flex items-center gap-6'>
+          <div className='relative h-16 w-16'>
+            <Image
+              src={`https://${process.env.NEXT_PUBLIC_RESOURCE_HOST}${targetImg}`}
+              alt={targetName}
+              className='rounded object-cover'
+              sizes='100%'
+              fill
+            />
+          </div>
+          <strong className='pb-5 text-xl'>{rank}</strong>
+          <section className='space-y-0.5'>
+            <h3 className='font-semibold tracking-wide'>{targetName}</h3>
+            <div className='text-sm tracking-wide opacity-50'>{entertainment}</div>
+          </section>
+        </CardContent>
+      </Card>
+    </li>
+  );
 };
 
 export default ChartItem;
