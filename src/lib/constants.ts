6d951a13d5fe8fda2d11bc09295f1680acb0027a
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2025-03-22 13:47:02 +0900

    feat: carousel, 배너 이미지 추가

---
+++ src/lib/constants.ts
@@ -0,0 +1,23 @@
+export const BANNERS = [
+  {
+    id: 1,
+    title: `[한터차트 공식]`,
+    sub: `르세라핌, ‘HOT'으로 초동 63만 장 돌파`,
+    link: 'https://hanteonews.com/ko/article/%EC%B5%9C%EC%8B%A0%EA%B8%B0%EC%82%AC?fc=77324',
+    image: '/images/le.png'
+  },
+  {
+    id: 2,
+    title: '32주년 한터뮤직어워즈 2024',
+    sub: `2025.02.15 - 16`,
+    link: 'https://awards.hanteo.com/?l=ko',
+    image: '/images/awards.png'
+  },
+  {
+    id: 3,
+    title: `[한터차트 공식]`,
+    sub: `제니, 'Ruby'로 초동 66만 장 돌파… ‘솔로 파워’ 입증`,
+    link: 'https://hanteonews.com/ko/article/chart?fc=76983',
+    image: '/images/jenny.png'
+  }
+];
